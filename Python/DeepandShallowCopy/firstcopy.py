afe20bc8996baf1bd05f06aa6be7b07aa7da4069
dineshkumarkb <dineshkumarkb@gmail.com>
2020-12-09 13:00:27 +0530

    copy changes

--- Python/DeepandShallowCopy/firstcopy.py
+++ Python/DeepandShallowCopy/firstcopy.py
@@ -5,8 +5,9 @@ l1 = [[1,2],2,3,4,5]
 #l2 = copy.copy(l1)
 l2 = copy.deepcopy(l1)
 
-#https://stackoverflow.com/questions/17246693/what-is-the-difference-between-shallow-copy-deepcopy-and-normal-assignment-oper/17246744#17246744
-
+# https://stackoverflow.com/questions/17246693/what-is-the-difference-between-shallow-copy-deepcopy-and-normal-assignment-oper/17246744#17246744
+# https://stackabuse.com/deep-vs-shallow-copies-in-python/
+# https://stackoverflow.com/questions/17246693/what-is-the-difference-between-shallow-copy-deepcopy-and-normal-assignment-oper/17246744#17246744
 print(l1, l2)
 print(id(l1), id(l2))
 print(id(l1[0]), id(l2[0]))
