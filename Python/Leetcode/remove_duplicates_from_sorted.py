ec833ac5e5c0f3916cf2e3ad636925236b3e207a
dineshkumarkb <dineshkumarkb@gmail.com>
2021-03-22 20:04:34 +0530

    Remove duplicates leet code

---
+++ Python/Leetcode/remove_duplicates_from_sorted.py
@@ -0,0 +1,27 @@
+"""
+Given a sorted array nums,
+remove the duplicates in-place such that each element appears only once and returns the new length.
+
+Do not allocate extra space for another array,
+you must do this by modifying the input array in-place with O(1) extra memory.
+"""
+
+
+class Solution:
+
+    def remove_duplicates(self, nums):
+
+        count = 0
+        for i in range(len(nums)-1):
+            if nums[count] == nums[count+1]:
+                nums.remove(nums[count])
+            else:
+                count += 1
+
+        print(len(nums))
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.remove_duplicates([0,0,1,1,1,2,2,3,3,4])
