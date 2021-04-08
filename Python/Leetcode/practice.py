34e63699def3e60f6a68ad29da4e2dc3a72726aa
dineshkumarkb <dineshkumarkb@gmail.com>
2021-04-08 09:39:16 +0530

    Delete node code added

--- Python/Leetcode/practice.py
+++ Python/Leetcode/practice.py
@@ -31,11 +31,37 @@ class LinkedList(object):
             print(current.data, end="->")
             current = current.next
 
+    def get_nodes_list(self):
+
+        current = self.head
+        nodes_lst = list()
+        while current:
+            nodes_lst.append(current)
+            current = current.next
+
+        return nodes_lst
+
+
+    def delete_node(self, node):
+
+        if node is None:
+            return None
+
+        next = node.next
+        node.data = next.data
+        node.next = next.next
+
+
 
 l = LinkedList()
 l.insert_node(10)
 l.insert_node(20)
 l.insert_node(30)
 l.insert_node(40)
+l.display_nodes()
+print()
+print(l.get_nodes_list()[2])
+
+l.delete_node(l.get_nodes_list()[2])
 l.display_nodes()
 
